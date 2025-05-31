70a1226f9f827fbc0116d601bcfffd7f710ad17a
Aryo <aryomuhammadputra@gmail.com>
2025-05-31 13:30:07 +0700

    feature: implement components in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-function App() {
+import { useState } from 'react';
+import SearchBar from '@/components/SearchBar';
+import UserList from '@/components/UserList';
+import mockUsers from '@/mock/mockUsers';
+
+export default function App() {
+    const [users, setUsers] = useState(mockUsers);
+
+    const handleSearch = async (_query: string) => {
+        setUsers(mockUsers);
+    };
+
     return (
-        <>
-            <h1>Hello</h1>
-        </>
+        <div className="max-w-md mx-auto mt-5 px-5">
+            <SearchBar
+                onSearch={handleSearch}
+                loading={false}
+            />
+            <UserList users={users} />
+        </div>
     );
 }
-
-export default App;
