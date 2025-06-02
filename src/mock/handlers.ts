0b990cc44a2f5decfc9f3894a0ac5a54ac49c214
Aryo <aryomuhammadputra@gmail.com>
2025-06-02 10:34:47 +0700

    chore: remove unused comments

--- src/mock/handlers.ts
+++ src/mock/handlers.ts
@@ -4,12 +4,10 @@ import mockUsers from './mockUsers';
 
 export const handlers = [
     rest.get('https://api.github.com/search/users', (req, res, ctx) => {
-        console.log('req bro : ', req);
         return res(ctx.status(200), ctx.json({ items: mockUsers }));
     }),
 
     rest.get('https://api.github.com/users/:username/repos', (req, res, ctx) => {
-        console.log('req bro : ', req);
         return res(ctx.status(200), ctx.json(mockRepos));
     }),
 ];
