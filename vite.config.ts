7c49e62c84a1adc21b0477da1382b0646982d471
Aryo <aryomuhammadputra@gmail.com>
2025-06-02 10:43:12 +0700

    feature: setting up github pgaes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    base: '/github-repositories-explorer/',
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
