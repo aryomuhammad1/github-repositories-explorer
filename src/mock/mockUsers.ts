47289ea1f5ebae7b66f4f560db0ad338b8292afc
Aryo <aryomuhammadputra@gmail.com>
2025-06-02 07:20:17 +0700

    refactor: update github mock data

--- src/mock/mockUsers.ts
+++ src/mock/mockUsers.ts
@@ -1,9 +1,9 @@
 import type { GitHubUser } from '@/types/github';
 
 const mockUsers: GitHubUser[] = [
-    { username: 'Exampleuser1', id: 1, url: '#' },
-    { username: 'Exampleuser431', id: 2, url: '#' },
-    { username: 'Exampleuser5655', id: 3, url: '#' },
+    { login: 'Exampleuser1', id: 1, url: '#' },
+    { login: 'Exampleuser431', id: 2, url: '#' },
+    { login: 'Exampleuser5655', id: 3, url: '#' },
 ];
 
 export default mockUsers;
